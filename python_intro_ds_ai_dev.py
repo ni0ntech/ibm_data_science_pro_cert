f8b86ad0d8dbefd80daf3291fc2d0e5ab4f552b3
ni0ntech <ni0ntech@pm.me>
2025-04-07 15:15:21 -0700

    Adding python file

---
+++ python_intro_ds_ai_dev.py
@@ -0,0 +1,8 @@
+#-----------------------------------------------------------------------------------
+# String interpolation (F-STRINGS):
+# String interpolation is a way to insert variables into strings.
+#-----------------------------------------------------------------------------------
+
+name = "Jack"
+age = 30
+print(f"My name is {name} and I am {age} years old.")
